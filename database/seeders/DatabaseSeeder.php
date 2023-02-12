7481c90923291d90ef29196c6bd0150027500b39
Himalayan <hdnpress@gmail.com>
2023-02-12 13:28:08 +0545

    Comment(s) my contain for both Post(s) and Video(s) & Seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,11 +19,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Post::factory(10)->create()->each(function ($post) {
+            $post->comments()
+                ->saveMany(Comment::factory(mt_rand(1, 2))->make());
+        });
+
+        Video::factory(10)->create()->each(function ($video) {
+            $video->comments()
+                ->saveMany(Comment::factory(mt_rand(1, 2))->make());
+        });
     }
 }
